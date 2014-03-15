a4832dd76952df344a9d34804b47efafa953133c
Artur Dzmitryieu (InhexSTER) <inhexster@gmail.com>
2014-03-15 08:25:45 -0400

    Remove a person from About dialog as per his request

--- DS4Tool/AboutBox.Designer.cs
+++ DS4Tool/AboutBox.Designer.cs
@@ -54,8 +54,8 @@
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(260, 155);
             this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "DS4 Tool 1.3 RC3\r\n\r\nCreated By:\r\n\r\nArtur Dzmitryieu(InhexSTER)\r\nHecticSeptic\r\nele" +
-    "ctrobrains\r\n\r\nBased on DS3 Scp Server from\r\nScarlet.Crush";
+            this.textBox1.Text = "DS4 Tool 1.3 RC3\r\n\r\nCreated By:\r\n\r\nArtur Dzmitryieu(InhexSTER)\r\nHecticSeptic\r\n\r\nB" +
+    "ased on DS3 Scp Server from\r\nScarlet.Crush";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // linkLabel1
